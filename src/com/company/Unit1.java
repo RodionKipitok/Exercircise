df4839293d561efa5fdedb6c985f5e594453939b
RodionKipitok <kucernurrabota@gmai.com>
2021-08-21 19:58:31 +0300

    My first branch

---
+++ src/com/company/Unit1.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Unit1 {
+    public static void main(String[] args) {
+
+        /**
+         * Урок 1. Hello World
+         *  Выведите в консоль сообщение: «Привет, мир!». +
+         *  Выведите в JOptionPane сообщение: «Я прохожу курс по Java!». +
+         *  Запустите написанное приложение в командной строке, перед этим его в командной
+         * строке скомпилировав. -+
+         *  Создайте jar-файл и проверьте его работу +
+         */
+        System.out.println("Hello Word");
+        JOptionPane.showMessageDialog(null,"  Я  прохожу  курс  по  Java");
+
+
+    }
+}
